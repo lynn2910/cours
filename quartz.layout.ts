b5aaf06d578278cd20ead71ae55d8ccefd78e4ff
--get <cedic2005@gmail.com>
2024-08-25 15:56:33 +0200

    Quartz sync: Aug 25, 2024, 3:56 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/lynn2910/",
+      Email: "mailto:cedric.colin35@gmail.com",
+      Portfolio: "https://lynn.chamallow.xyz",
     },
   }),
 }
